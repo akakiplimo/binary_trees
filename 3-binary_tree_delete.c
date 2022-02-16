036eee35336a481a06af223da38e435f283ab9d3
akakiplimo <adriankiplimo@gmail.com>
2022-02-16 10:59:47 +0000

    recursive del call

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -12,8 +12,8 @@ void binary_tree_delete(binary_tree_t *tree)
 if (tree == NULL)
 return;
 
-binary_tree_delete(tree->left) = NULL;
-binary_tree_delete(tree->right) = NULL;
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
 
 free(tree);
 }
