5cb3fddbb4304de948a7ed0dd271099cce1c8503
akakiplimo <adriankiplimo@gmail.com>
2022-02-17 07:00:46 +0000

    Betty updates

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,15 +8,15 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  size_t left = 0;
-  size_t right = 0;
+size_t left = 0;
+size_t right = 0;
 
-  if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-    return (0);
+if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+return (0);
 
-  left = binary_tree_height(tree->left);
-  right =binary_tree_height(tree->right);
+left = binary_tree_height(tree->left);
+right =binary_tree_height(tree->right);
 
-  /*IF... ELSE shortform in C*/
-  return ((left > right ? left : right) + 1);
+/*IF... ELSE shortform in C*/
+return ((left > right ? left : right) + 1);
 }
