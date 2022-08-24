c0a3c6ad5cfb1bc98120e2ee729733f9ee3178d8
ritakendi <ritabetty77@gmail.com>
2022-08-24 07:17:11 -0700

    [modified]Level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -6,22 +6,22 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t l = 0;
-    size_t r = 0;
+	size_t l = 0;
+	size_t r = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        if (tree)
-        {
-            l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-            r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-        }
-        return ((l > r) ? l : r);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
 /**
  * binary_tree_depth - depth of specified node from root
@@ -30,7 +30,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
 /**
  * linked_node - this function makes a linked list from depth level and node
@@ -41,30 +41,30 @@ size_t binary_tree_depth(const binary_tree_t *tree)
  */
 void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
 {
-    link_t *new, *aux;
+	link_t *new, *aux;
 
-    new = malloc(sizeof(link_t));
-    if (new == NULL)
-    {
-        return;
-    }
-    new->n = level;
-    new->node = tree;
-    if (*head == NULL)
-    {
-        new->next = NULL;
-        *head = new;
-    }
-    else
-    {
-        aux = *head;
-        while (aux->next != NULL)
-        {
-            aux = aux->next;
-        }
-        new->next = NULL;
-        aux->next = new;
-    }
+	new = malloc(sizeof(link_t));
+	if (new == NULL)
+	{
+		return;
+	}
+	new->n = level;
+	new->node = tree;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		aux = *head;
+		while (aux->next != NULL)
+		{
+			aux = aux->next;
+		}
+		new->next = NULL;
+		aux->next = new;
+	}
 }
 /**
  * recursion - goes through the complete tree and each stores each node
@@ -75,15 +75,15 @@ void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
  */
 void recursion(link_t **head, const binary_tree_t *tree)
 {
-    size_t level = 0;
+	size_t level = 0;
 
-    if (tree != NULL)
-    {
-        level = binary_tree_depth(tree);
-        linked_node(head, tree, level);
-        recursion(head, tree->left);
-        recursion(head, tree->right);
-    }
+	if (tree != NULL)
+	{
+		level = binary_tree_depth(tree);
+		linked_node(head, tree, level);
+		recursion(head, tree->left);
+		recursion(head, tree->right);
+	}
 }
 /**
  * binary_tree_levelorder - print the nodes element in a lever-order
@@ -93,36 +93,36 @@ void recursion(link_t **head, const binary_tree_t *tree)
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    link_t *head, *aux;
-    size_t height = 0, count = 0;
+	link_t *head, *aux;
+	size_t height = 0, count = 0;
 
-    if (!tree || !func)
-    {
-        return;
-    }
-    else
-    {
-        height = binary_tree_height(tree);
-        head = NULL;
-        recursion(&head, tree);
-        while (count <= height)
-        {
-            aux = head;
-            while (aux != NULL)
-            {
-                if (count == aux->n)
-                {
-                    func(aux->node->n);
-                }
-                aux = aux->next;
-            }
-            count++;
-        }
-        while (head != NULL)
-        {
-            aux = head;
-            head = head->next;
-            free(aux);
-        }
-    }
+	if (!tree || !func)
+	{
+		return;
+	}
+	else
+	{
+		height = binary_tree_height(tree);
+		head = NULL;
+		recursion(&head, tree);
+		while (count <= height)
+		{
+			aux = head;
+			while (aux != NULL)
+			{
+				if (count == aux->n)
+				{
+					func(aux->node->n);
+				}
+				aux = aux->next;
+			}
+			count++;
+		}
+		while (head != NULL)
+		{
+			aux = head;
+			head = head->next;
+			free(aux);
+		}
+	}
 }
