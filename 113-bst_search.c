bc0e2aed108009ca2d1e9bac432116e98dcc926a
ritakendi <ritabetty77@gmail.com>
2022-08-25 01:06:19 -0700

    [modified]BST - Search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -9,11 +9,11 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-    if (tree && value < tree->n)
-        return (bst_search(tree->left, value));
+	if (tree && value < tree->n)
+		return (bst_search(tree->left, value));
 
-    if (tree && value > tree->n)
-        return (bst_search(tree->right, value));
+	if (tree && value > tree->n)
+		return (bst_search(tree->right, value));
 
-    return ((bst_t *)tree);
+	return ((bst_t *)tree);
 }
